95dae817d5f8781f7243c64c05b5bccf857bc5f0
Pello Usabiaga <pellousabiaga@gmail.com>
2025-04-05 14:05:01 +0200

    Euler backward programmed by gpt

--- src/ContinuousAndOde/EulerBackwardStepSolver.cpp
+++ src/ContinuousAndOde/EulerBackwardStepSolver.cpp
@@ -7,19 +7,60 @@ namespace PySysLinkBase
                                                                                 std::function<std::vector<std::vector<double>>(std::vector<double>, double)> systemJacobian,
                                                                                 std::vector<double> states_0, double currentTime, double timeStep)
     {
-        std::vector<double> gradient = systemDerivatives(states_0, currentTime);
-        spdlog::get("default_pysyslink")->debug("Gradient size: {}", gradient.size());
-        for (const auto& value: gradient)
-        {
-            spdlog::get("default_pysyslink")->debug(value);
-        }
+        const double tol = 1e-6;
+        const int maxIter = 50;
+        double t_next = currentTime + timeStep;
+        int n = states_0.size();
+        std::vector<double> x = states_0;  // initial guess
 
-        std::vector<double> newStates(gradient.size(), 0.0);
-        for (int i = 0; i < gradient.size(); i++)
-        {
-            newStates[i] = states_0[i] + gradient[i] * timeStep;
+        // Newton-Raphson iteration
+        for (int iter = 0; iter < maxIter; ++iter) {
+            // Evaluate function f(x, t_next)
+            std::vector<double> f = systemDerivatives(x, t_next);
+            // Compute F(x) = x - states_0 - timeStep * f(x, t_next)
+            std::vector<double> F(n, 0.0);
+            for (int i = 0; i < n; i++) {
+                F[i] = x[i] - states_0[i] - timeStep * f[i];
+            }
+            // Compute norm of F(x)
+            double normF = 0.0;
+            for (double val : F) {
+                normF += val * val;
+            }
+            normF = std::sqrt(normF);
+            spdlog::get("default_pysyslink")->debug("Iteration {}: normF = {}", iter, normF);
+            if (normF < tol) {
+                return {true, x, timeStep};
+            }
+            // Compute Jacobian J(x, t_next)
+            std::vector<std::vector<double>> J = systemJacobian(x, t_next);
+            // Form matrix A = I - timeStep * J
+            std::vector<std::vector<double>> A(n, std::vector<double>(n, 0.0));
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == j) {
+                        A[i][j] = 1.0 - timeStep * J[i][j];
+                    } else {
+                        A[i][j] = - timeStep * J[i][j];
+                    }
+                }
+            }
+            // Solve linear system A * delta = F
+            std::vector<double> delta = this->SolveLinearSystem(A, F);
+            // Update x: x = x - delta
+            double normDelta = 0.0;
+            for (int i = 0; i < n; i++) {
+                x[i] -= delta[i];
+                normDelta += delta[i] * delta[i];
+            }
+            normDelta = std::sqrt(normDelta);
+            spdlog::get("default_pysyslink")->debug("Iteration {}: normDelta = {}", iter, normDelta);
+            if (normDelta < tol) {
+                return {true, x, timeStep};
+            }
         }
-        return {true, newStates, timeStep};
+        spdlog::get("default_pysyslink")->error("Newton-Raphson did not converge after {} iterations", maxIter);
+        return {false, x, timeStep};
     }
 
 } // namespace PySysLinkBase
--- src/ContinuousAndOde/EulerBackwardStepSolver.h
+++ src/ContinuousAndOde/EulerBackwardStepSolver.h
@@ -5,7 +5,11 @@
 #include <tuple>
 #include <vector>
 #include <functional>
+#include <sstream>
 #include "IOdeStepSolver.h"
+#include <cmath>
+#include <stdexcept>
+#include <iostream>
 
 namespace PySysLinkBase
 {
@@ -15,7 +19,7 @@ namespace PySysLinkBase
             virtual std::tuple<bool, std::vector<double>, double> SolveStep(std::function<std::vector<double>(std::vector<double>, double)> system, 
                                                                     std::vector<double> states_0, double currentTime, double timeStep)
             {
-                throw std::runtime_error("Jacobian needed");
+                throw std::runtime_error("Jacobian needed for implicit Euler method");
             }
             virtual std::tuple<bool, std::vector<double>, double> SolveStep(std::function<std::vector<double>(std::vector<double>, double)> systemDerivatives,
                                                                     std::function<std::vector<std::vector<double>>(std::vector<double>, double)> systemJacobian, 
@@ -23,6 +27,54 @@ namespace PySysLinkBase
             virtual bool IsJacobianNeeded() const 
             {
                 return true;
+            }
+
+            // A simple solver for a linear system using Gaussian elimination
+            std::vector<double> SolveLinearSystem(const std::vector<std::vector<double>>& A, const std::vector<double>& b)
+            {
+                int n = A.size();
+                std::vector<std::vector<double>> M = A; // copy of A
+                std::vector<double> x = b;              // copy of b (will hold modified RHS)
+
+                // Gaussian elimination with partial pivoting
+                for (int i = 0; i < n; i++) {
+                    // find pivot
+                    int pivot = i;
+                    double maxVal = std::fabs(M[i][i]);
+                    for (int j = i + 1; j < n; j++) {
+                        if (std::fabs(M[j][i]) > maxVal) {
+                            maxVal = std::fabs(M[j][i]);
+                            pivot = j;
+                        }
+                    }
+                    if (std::fabs(M[pivot][i]) < 1e-12) {
+                        std::ostringstream oss;
+                        oss << "Matrix is singular at row " << i;
+                        throw std::runtime_error(oss.str());
+                    }
+                    if (pivot != i) {
+                        std::swap(M[i], M[pivot]);
+                        std::swap(x[i], x[pivot]);
+                    }
+                    // eliminate column i
+                    for (int j = i + 1; j < n; j++) {
+                        double factor = M[j][i] / M[i][i];
+                        for (int k = i; k < n; k++) {
+                            M[j][k] -= factor * M[i][k];
+                        }
+                        x[j] -= factor * x[i];
+                    }
+                }
+                // Back substitution
+                std::vector<double> solution(n, 0.0);
+                for (int i = n - 1; i >= 0; i--) {
+                    double sum = 0.0;
+                    for (int j = i + 1; j < n; j++) {
+                        sum += M[i][j] * solution[j];
+                    }
+                    solution[i] = (x[i] - sum) / M[i][i];
+                }
+                return solution;
             }
     };
 } // namespace PySysLinkBase
